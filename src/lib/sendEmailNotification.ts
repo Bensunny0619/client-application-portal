2dd88d6c658de2e173c5b3e56d5c704e53d5049e
Gbenga Emmanuel <98877287+Bensunny0619@users.noreply.github.com>
2025-08-04 01:10:47 +0100

    fixed some more errors

--- src/lib/sendEmailNotification.ts
+++ src/lib/sendEmailNotification.ts
@@ -1,28 +1,28 @@
-// lib/sendEmailNotification.ts
-import emailjs from "@emailjs/browser";
+// // lib/sendEmailNotification.ts
+// import emailjs from "@emailjs/browser";
 
-export const sendEmailNotification = async ({
-  toEmail,
-  clientname,
-  formtype,
-  status,
-}) => {
-  const templateParams = {
-    to_email: toEmail,
-    clientname,
-    formtype,
-    status,
-  };
+// export const sendEmailNotification = async ({
+//   toEmail,
+//   clientname,
+//   formtype,
+//   status,
+// }) => {
+//   const templateParams = {
+//     to_email: toEmail,
+//     clientname,
+//     formtype,
+//     status,
+//   };
 
-  try {
-    const result = await emailjs.send(
-     process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
-      templateParams,
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
-    );
-    console.log("Email sent successfully", result.status);
-  } catch (error) {
-    console.error("Email sending failed", error);
-  }
-};
+//   try {
+//     const result = await emailjs.send(
+//      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+//       process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+//       templateParams,
+//       process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+//     );
+//     console.log("Email sent successfully", result.status);
+//   } catch (error) {
+//     console.error("Email sending failed", error);
+//   }
+// };
